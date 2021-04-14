02a9ae1e91481fa7e3ae97b8194e8693b1354da7
infraendboss <47447942+infraendboss@users.noreply.github.com>
2021-04-14 13:19:58 +0200

    Extra bron toevoegen

--- VA2.py
+++ VA2.py
@@ -1,3 +1,6 @@
+#Voor extra informatie kan de README file gelezen worden
+#Bronnen: 
+
 import os
 import time
 import playsound
@@ -208,3 +211,57 @@ if __name__ == '__main__':
             except Exception as e:                              #email kan niet verstuurd worden
                 print(e)
                 speak("ik kan de mail niet versturen")       #jarvis bevestigd
+
+        elif "weer" in text:
+             
+            # Google Open weather website
+            # to get API of Open weather
+            api_key = "b7688b61e3c305e8d97e59c2398e74d4"
+            base_url = "https://api.openweathermap.org/data/2.5/weather?"
+            speak(" Stad naam ")
+            print("Stad naam : ")
+            city_name = get_audio()
+            complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+            response = requests.get(complete_url)
+            x = response.json()
+
+            if x["cod"] != "404":
+  
+                # store the value of "main"
+                # key in variable y
+                y = x["main"]
+            
+                # store the value corresponding
+                # to the "temp" key of y
+                current_temperature = y["temp"]
+                current_temperature = current_temperature - 273.15
+            
+                # store the value corresponding
+                # to the "pressure" key of y
+                current_pressure = y["pressure"]
+            
+                # store the value corresponding
+                # to the "humidity" key of y
+                current_humidiy = y["humidity"]
+            
+                # store the value of "weather"
+                # key in variable z
+                z = x["weather"]
+            
+                # store the value corresponding 
+                # to the "description" key at 
+                # the 0th index of z
+                weather_description = z[0]["description"]
+            
+                # print following values
+                speak(" Temperatuur = " +
+                                str(current_temperature) + 
+                    "\n De luchtdruk is  = " +
+                                str(current_pressure) +
+                    "\n Regenverwachting is  = " +
+                                str(current_humidiy) +
+                    "\n Weersbeschrijving = " +
+                                str(weather_description))
+            
+        else:
+            print(" City Not Found ")
